bc3777b0f398c996bc4321be8ce33e61d7cf94aa
Hassan752 <66700578+Hassan752@users.noreply.github.com>
2021-05-24 16:50:11 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
-	<title> User Login And Registration</title>
+	<title> User Login And Registeration</title>
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -12,41 +12,43 @@
 		    <form action="validation.php" method="post"> 
 		       <div class="form-group">
 		           <label>Username</label>
-                   <label>
-                       <input type="text" name="user" class="form-control" required>
-                   </label>
-
-               </div>
+		              <input type="text" name="user" class="form-control" required>
+			
+		</div>
 		<div class="form-group">
 		<label>Password</label>
-            <label>
-                <input class="form-control" name="password" required type="password">
-            </label>
+		<input type="password" name="password" class="form-control" required>
+
+			
 
 
-        </div>
+
+
+
+		</div>
 		<button type="submit" class="btn btn-primary" >Login</button>
 		</form>
 		</div>
 		
 		<div class="col-md-6">
 		   <h2> Register Here </h2>	
-		    <form action="registration.php" method="post">
+		    <form action="registeration.php" method="post"> 
 		       <div class="form-group">
 		           <label>Username</label>
-                   <label>
-                       <input type="text" name="user" class="form-control" required>
-                   </label>
-
-               </div>
+		              <input type="text" name="user" class="form-control" required>
+			
+		</div>
 		<div class="form-group">
 		<label>Password</label>
-            <label>
-                <input type="password" name="password" class="form-control" required>
-            </label>
+		<input type="password" name="password" class="form-control" required>
+
+			
 
 
-        </div>
+
+
+
+		</div>
 		<button type="submit" class="btn btn-primary" >Register
 		</div>
 
